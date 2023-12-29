659e1963413eba24b5fe2907a45d210e4d67695b
Gustavo-Vinicius <gustavovinicimos12345@gmail.com>
2023-12-29 17:26:39 -0300

    feat : adicionando novos campos na classe Cliente e aplicando migration.

---
+++ Consultorio_Legal.DATA/Migrations/20231229125329_AdicionandoCamposCliente.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Consultorio_Legal.DATA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoCamposCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Documento",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sexo",
+                table: "Clientes",
+                type: "nvarchar(1)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Sexo",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Clientes");
+        }
+    }
+}
